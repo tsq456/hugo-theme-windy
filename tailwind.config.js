c799a91b3e833c770bd40023f13e514b278df331
tsq <115685>
2021-07-21 20:30:01 +0800

    modify homepage style

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,16 @@
 module.exports = {
     mode: "jit",
     purge: ["./content/**/*.md", "./content/**/*.html", "./themes/hugo-theme-windy/layouts/**/*.html"],
+
+    theme: {
+      extend: {
+        colors: {
+          'accent': '#fc4041',
+        }
+      }
+    },
+
+    plugins: [
+      require('@tailwindcss/typography'),
+    ],
   };
