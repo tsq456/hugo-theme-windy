513db8b69c435c9564eb184c3211bb6411779720
tsq <115685>
2021-07-28 19:02:14 +0800

    modify

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,15 +15,35 @@ module.exports = {
         lg: '0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)',
         xl: '0 20px 25px -5px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.04)',
         inner: 'inset 0 2px 4px 0 rgba(0, 0, 0, 0.06)',
-        'nav': 'inset 0 1rem 0 0 #fff, inset 0 -2rem 0 0 rgb(252 64 65 / 40%)',
+        'lightMenuList': 'inset 0 1rem 0 0 #fff, inset 0 -2rem 0 0 rgb(252 64 65)',
+        'darkMenuList': 'inset 0 1rem 0 0 #1f2937, inset 0 -2rem 0 0 rgb(252 64 65)',
         'bg': '0 0 10px rgb(0 0 0 / 10%)',
       },
       extend: {
         colors: {
           'accent': '#fc4041',
           'blackGradient': 'rgb(0,0,0,.7)',
+          'darkBgDarker': '#121212',
+          'darkBglighter': '#272727',
+          'darkPrimary': '#e0e0e0',
+          'darkSecondry': '#a0a0a0',
+          'darkDivider': '#5c5c5c',
         },
         typography: {
+          'dark': {
+            css: {
+              'h2, h3, p, strong, li, code, code>span, li:before': {
+                color: '#e0e0e0',
+              },
+              a: {
+                boxShadow: 'inset 0 1em 0 0 #1f2937, inset 0 -0.375em 0 0 rgb(252 64 65)',
+                '&:hover': {
+                  boxShadow: 'inset 0 .8em 0 0 #1f2937, inset 0 -0.375em 0 0 rgb(252 64 65)',
+                  color: '#e2e2e2',
+                }
+              },
+            }
+          },
           DEFAULT: {
             css: {
               a: {
@@ -73,6 +93,12 @@ module.exports = {
       }
     },
 
+    variants: {
+      extend: {
+        textOpacity: ['dark']
+      }
+    },
+    
     plugins: [
       require('@tailwindcss/typography'),
     ],
